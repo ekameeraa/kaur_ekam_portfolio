7e5f4ccbaa2d9dcdc404c69f386977b4108fe13e
ekampreetkaur <113371633+ekameeraa@users.noreply.github.com>
2023-12-09 16:55:26 -0500

    updating and fixing

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-var elmntToView = document.getElementById("sectionId");
-elmntToView.scrollIntoView();
+// var elmntToView = document.getElementById("sectionId");
+// elmntToView.scrollIntoView();
 
-$("p a").click(function (e) {
-  e.preventDefault();
-  var link = $(this).attr("href");
-});
+// $("p a").click(function (e) {
+//   e.preventDefault();
+//   var link = $(this).attr("href");
+// });
 
-document.getElementById("home").addEventListener("click", scrollfunc);
+// document.getElementById("home").addEventListener("click", scrollfunc);
 
-function scrollfunc() {
-  // code to be executed
-  var elmntToView = document.getElementById("section1");
-  elmntToView.scrollIntoView();
-}
+// function scrollfunc() {
+ 
+//   var elmntToView = document.getElementById("section1");
+//   elmntToView.scrollIntoView();
+// }
 
 
