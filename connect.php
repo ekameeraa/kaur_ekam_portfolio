4cace0942085d8361c3ed7c4c038ff61c63233c1
ekampreetkaur <113371633+ekameeraa@users.noreply.github.com>
2023-12-10 17:04:57 -0500

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,27 +1,28 @@
 <?php
-
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$firstName = $_POST['firstName'];
-$contactInfo = $_POST['contactInfo'];
-$msg = $_POST['msg'];
-$submit = $_POST['submit'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $firstName = $_POST['firstname'];
+    $lastName = $_POST['lastname'];
+    $country = $_POST['country'];
+    $subject = $_POST['subject'];
 
-$connect = new mysqli('localhost', 'root', 'root', 'EMAILdb');
+    $connect = new mysqli('localhost', 'root', 'root', 'EMAILdb');
 
-if ($connect->connect_error) {
-    die('Connection failed: ' . $connect->connect_error);
+    if ($connect->connect_error) {
+        die('Connection failed: ' . $connect->connect_error);
+    }
+
+    $sql = "INSERT INTO EMAILdb (firstname, lastname, country, subject) VALUES ('$firstName', '$lastName', '$country', '$subject')";
+
+    if ($connect->query($sql) === TRUE) {
+        echo "Record inserted successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $connect->error;
+    }
+
+    $connect->close();
 }
-
-$sql = "INSERT INTO EMAILdb (firstname, contactinfo, msg, submit) VALUES ('$firstName', '$contactInfo', '$msg', '$submit')";
-
-if ($connect->query($sql) === TRUE) {
-    echo "Record inserted successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $connect->error;
-}
-
-$connect->close();
-
 ?>
+
